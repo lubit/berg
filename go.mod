650b998466b8ded49633483424b6a944490e8611
peilin <peilin@staff.weibo.com>
2019-09-09 20:26:24 +0800

    build

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/hashicorp/memberlist v0.1.4
+	github.com/hashicorp/serf v0.8.3
+	github.com/mitchellh/cli v1.0.0
 	github.com/segmentio/kafka-go v0.3.3
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
 	google.golang.org/grpc v1.23.0
--- testModel.go
+++ testModel.go
@@ -6,27 +6,27 @@ import (
 	//"github.com/lubit/berg/models"
 )
 
-func main(){
+func TestModel() {
 	//application := models.NewJob()
 	////application.AppChannels.AddChannel()
 	//
 	//application.AppSource.Execute(application.AppChannels)
 
 	var tmpslice []int
-	tmpslice = append(tmpslice,1)
-	tmpslice = append(tmpslice,2)
+	tmpslice = append(tmpslice, 1)
+	tmpslice = append(tmpslice, 2)
 
-	fmt.Println(tmpslice[0],tmpslice[1])
+	fmt.Println(tmpslice[0], tmpslice[1])
 
 	//fmt.Printf("New Application Initiated:%v",application)
 
 	a()
 
-	inchan := make(chan int,1)
+	inchan := make(chan int, 1)
 
 	inchan <- 1
 
-	ctx,cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 
 	//go func(){
 	//	defer cancel()
@@ -34,11 +34,10 @@ func main(){
 	//	fmt.Println("inside")
 	//}()
 
-	fmt.Println(ctx,cancel)
+	fmt.Println(ctx, cancel)
 	//fearless-ry added for name test
 	//context
 }
-
 
 func a() {
 	var i int
@@ -50,5 +49,5 @@ func a() {
 		i++
 		fmt.Println("defer1:", i) // 打印结果为 defer: 1
 	}()
-	fmt.Println("value i:",i)
+	fmt.Println("value i:", i)
 }
